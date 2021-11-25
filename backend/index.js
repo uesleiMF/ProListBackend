c122223c6117b011d90b51ce01237c5f1eb1e495
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2021-11-24 21:39:16 -0300

    sim

--- backend/index.js
+++ backend/index.js
@@ -22,8 +22,7 @@ const db_data = process.env.DB_DATA;
 Conn(db_url,db_user,db_pass,db_data);
 
 const port = 3001;
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`O servidor esta rodando na porta ${port}`);
 })
-
 
