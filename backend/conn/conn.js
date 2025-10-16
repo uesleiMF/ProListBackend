f56b92e956c2f2edf07452c0f7f8a2814fff0d33
Jose Ueslei De Moraes Ferreira <joseueslei27@gmail.com>
2025-10-16 10:40:46 -0300

    hoje

--- backend/conn/conn.js
+++ backend/conn/conn.js
@@ -1,18 +1,22 @@
 const mongoose = require('mongoose');
 
-const Conn = (url, user, pass, data)=>{
-  
-  mongoose.connect(`${url}/${data}`,{
-      user:user,
-      pass:pass,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  ).then(() => {
-    console.log('Conexao com o MongoDB executada com sucesso');
-  }).catch((err) => {
-    console.error(err);
+const Conn = (url, user, pass, data) => {
+  let fullUrl;
+
+  if(user && pass) {
+    // Mongo Atlas
+    fullUrl = `mongodb+srv://${user}:${pass}@${url}/${data}?retryWrites=true&w=majority`;
+  } else {
+    // Mongo local
+    fullUrl = `${url}/${data}`;
+  }
+
+  mongoose.connect(fullUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
-}
+  .then(() => console.log('✅ Conectado ao MongoDB'))
+  .catch(err => console.error('❌ Erro ao conectar ao MongoDB:', err.message));
+};
 
 module.exports = Conn;
